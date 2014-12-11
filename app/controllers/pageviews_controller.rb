bde8a28565da8a2f07a8461138349a117cc59208
Kandis <ktagliabue@hotmail.com>
2014-12-11 15:04:15 -0800

    daily charts

--- app/controllers/pageviews_controller.rb
+++ app/controllers/pageviews_controller.rb
@@ -1,5 +1,7 @@
 class PageviewsController < ApplicationController
   def index
     @pageviews = Pageview.all
+    params[:datetime] ||= Time.now.to_s
+    @date = Time.parse(params[:datetime])
   end
 end
